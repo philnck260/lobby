1e5c98c3234e7d31f114954e8efa5a728e7314c5
axaidan <axaidan@hotmail.com>
2020-03-17 17:03:42 +0100

    changed root_path in config/routes.rb, edited _navbar partial with root_path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
 	resources :commitments, only: [:show, :index]
+	root to: "commitments#index"
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
