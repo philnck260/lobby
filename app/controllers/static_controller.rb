5a8646e1c1b9fe6c7d25c95b671944c3ca04d90e
amine91480 <amine91480@hotmail.fr>
2020-03-17 16:17:14 +0100

    Add Formulaire to contact

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,13 +1,16 @@
 class StaticController < ApplicationController
   def contact
     render params[:page]
+    puts params
   end
 
   def faq
     render params[:page]
+    puts params
   end
 
   def about
     render params[:page]
+    puts params
   end
 end
