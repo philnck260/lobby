7c01379b6e56d974e03390a8077cd52d9053858a
axaidan <axaidan@hotmail.com>
2020-03-18 12:49:47 +0100

    modified app/mailers/admin_mailer.rb which was probably the cause for heroku crash

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -6,7 +6,6 @@ class AdminMailer < ApplicationMailer
         #on définit une variable @url qu'on utilisera dans la view d'e-mail
         @url  = 'http://monsite.fr/login' 
         # c'est cet appel à mail() qui permet d'envoyer l'e-mail en définissant destinataire et sujet.
-        User.where(role: "admin").each do |admin|
-            mail(to: admin.email, subject: 'Bienvenue chez nous !') 
-      end
+        mail(to: 'admilobbynistrator@gmail.com', subject: 'static contact mail') 
+		end
 end
