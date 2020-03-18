8ffc152954eb76b613f0764f6dbc8bab9eb68981
Daniel <daniel.venera2@gmail.com>
2020-03-18 17:38:03 +0100

    dashboard admin users & commitments with controllers, views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
+  resources :commitments, only: [:show, :index] do
+    resources :user_commitments, only: [:create, :destroy]
+  end
+  root to: "commitments#index"
 
-	resources :commitments, only: [:show, :index] do
-		resources :user_commitments, only: [:create, :destroy]
-	end
-	root to: "commitments#index"
+  get "/Contact" => "static#contact"
+  get "/About" => "static#about"
+  get "/FAQ" => "static#faq"
 
+  devise_for :users, controllers: { registrations: "registrations" }
+  resources :users, only: [:show, :edit, :update]
 
-	get '/Contact' => 'static#contact'
-	get '/About' => 'static#about'
-	get '/FAQ' => 'static#faq'
+  scope "admin", module: "admin", as: "admin" do
+    resources :commitments
+    resources :users
+  end
 
-	devise_for :users, controllers: { registrations: "registrations" }
-	resources :users, only: [:show, :edit, :update]
-
-
-	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
