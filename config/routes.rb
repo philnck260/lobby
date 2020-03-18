600edf5f122b7c72aaaf5f5f2fc3f6aa96d29ac8
mathieu <mathieu.lasserre2@gmail.com>
2020-03-18 17:53:05 +0100

    Contact mailer to admin v1

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,12 @@ Rails.application.routes.draw do
 	end
 	root to: "commitments#index"
 
-
-	get '/Contact' => 'static#contact'
-	get '/About' => 'static#about'
-	get '/FAQ' => 'static#faq'
+	get '/about' => 'static#about'
+	get '/faq' => 'static#faq'
 
 	devise_for :users, controllers: { registrations: "registrations" }
 	resources :users, only: [:show, :edit, :update]
-
+	resources :contact, only: [:new, :create]
 
 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
