ea1950225f7dc3d19348850f11c00113799a6e02
amine91480 <amine91480@hotmail.fr>
2020-03-19 18:09:23 +0100

    V2 Contact Mailer To Send at all Admin, if user_signe -> The mail was sent with his acocunt_mail, add subject, change view of the mail, add class

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -6,8 +6,12 @@ class ContactController < ApplicationController
 
     def create
       email = params[:email]
+      subject = params[:subject]
       content = params[:content]
-      ContactMailer.contact_email(email, content).deliver
+      if user_signed_in?
+        email = current_user.email
+      end
+        ContactMailer.contact_email(email,content,subject).deliver
       redirect_to root_path
     end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,5 @@
 class Contact < ActionMailer::Base
-  after_action :contact_send
-  
+
     def contact_send 
       ContactMailer.contact_email(self).deliver_now
     end
