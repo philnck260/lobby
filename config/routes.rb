73c48aa16a5489369e5695e7421c2593a7df5cec
axaidan <axaidan@hotmail.com>
2020-03-23 17:43:55 +0100

    follow theme btn working for user signed_in and invisible for visitor

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
 
 	devise_for :users, controllers: { registrations: "registrations" }
 	resources :users, only: [:show, :edit, :update]
-	resources :themes, only: [:show, :index]
+	resources :themes, only: [:show, :index] do
+		resources :user_themes, only: [:create, :destroy]
+	end
 	resources :contact, only: [:new, :create]
 
 	resources :posts 
