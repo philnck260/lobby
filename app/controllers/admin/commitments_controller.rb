d4f340e15b9939bd812275844f81285687054d8a
axaidan <axaidan@hotmail.com>
2020-03-24 12:51:29 +0100

    YEEES commitment creation working with 1 Theme, second optionnal theme to be implemented in another feature

--- app/controllers/admin/commitments_controller.rb
+++ app/controllers/admin/commitments_controller.rb
@@ -11,20 +11,16 @@ module Admin
 		def new
 			@themes = themes_titles 
 			@commitment = Commitment.new
-				@commitment.commitment_themes.build 
-				@commitment.commitment_themes.each do |ct|
-					ct.build_theme
-
+			@commitment.commitment_themes.build 
+			@commitment.commitment_themes.each do |ct|
+				ct.build_theme
 			end
 		end
 
 		def create
-			puts "=" * 60 + "\n" + commitment_params[:commitment_themes_attributes]["0"][:theme].to_s + "\n" + "=" * 60
 			@commitment = Commitment.new(commitment_params)
-			ct = CommitmentTheme.new(commitment: @commitment, theme: Theme.find(commitment_params[:commitment_themes_attributes]["0"][:theme_id]))
-			
 			if @commitment.save
-				flash[:success] = "Vous avez créé un mouvement"
+				flash[:success] = "Vous avez créé un Mouvement"
 				redirect_to admin_commitments_path
 			else
 				flash[:error] = @commitment.errors.full_messages.to_sentence
@@ -37,17 +33,17 @@ module Admin
 
 		def update
 			if @commitment.update(commitment_params)
-			flash[:success] = 'Vous avez bien édité ce mouvement'
-			redirect_to admin_commitments_path
+				flash[:success] = 'Vous avez bien édité ce Mouvement'
+				redirect_to admin_commitments_path
 			else
-			flash[:error] = @commitment.errors.full_messages.to_sentence
-			redirect_to edit_admin_commitment_path(@commitment.id)
+				flash[:error] = @commitment.errors.full_messages.to_sentence
+				redirect_to edit_admin_commitment_path(@commitment.id)
 			end
 		end
 
 		def destroy
 			@commitment.destroy
-			flash[:success] = 'Vous avez bien supprimé ce mouvement'
+			flash[:success] = 'Vous avez bien supprimé ce Mouvement'
 			redirect_to admin_commitments_path
 		end
 
