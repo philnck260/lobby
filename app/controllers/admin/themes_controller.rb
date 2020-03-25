97b0c09b858ba38d459510badb61788bddd643e1
Daniel <daniel.venera2@gmail.com>
2020-03-25 10:57:16 +0100

    add ajax request for destroy in admin themes (custom flash message)

--- app/controllers/admin/themes_controller.rb
+++ app/controllers/admin/themes_controller.rb
@@ -1,64 +1,67 @@
 module Admin
-	class ThemesController < ApplicationController
-		before_action :find_theme, only: [:edit, :update, :destroy]
-		before_action :authenticate_user!
-		before_action :is_user_admin?
+  class ThemesController < ApplicationController
+    before_action :find_theme, only: [:edit, :update, :destroy]
+    before_action :authenticate_user!
+    before_action :is_user_admin?
 
-		def index
-			@themes = Theme.all
-		end
+    def index
+      @themes = Theme.all
+    end
 
-		def new
-			@theme = Theme.new
-		end
+    def new
+      @theme = Theme.new
+    end
 
-		def create
-			@theme = Theme.new(theme_params)
-			if @theme.save
-				flash[:success] = "Vous avez créé un Thème."
-				redirect_to admin_commitments_path
-			else
-				flash[:error] = @theme.errors.full_messages.to_sentence
-				redirect_to new_admin_theme_path
-			end
-		end
+    def create
+      @theme = Theme.new(theme_params)
+      if @theme.save
+        flash[:success] = "Vous avez créé un Thème."
+        redirect_to admin_commitments_path
+      else
+        flash[:error] = @theme.errors.full_messages.to_sentence
+        redirect_to new_admin_theme_path
+      end
+    end
 
-		def edit
-		end
+    def edit
+    end
 
-		def update
-			if @theme.update(theme_params)
-				flash[:success] = 'Vous avez bien édité ce Thème.'
-				redirect_to admin_themes_path
-			else
-				flash[:error] = @theme.errors.full_messages.to_sentence
-				redirect_to edit_admin_theme_path(@theme.id)
-			end
-		end
+    def update
+      if @theme.update(theme_params)
+        flash[:success] = "Vous avez bien édité ce Thème."
+        redirect_to admin_themes_path
+      else
+        flash[:error] = @theme.errors.full_messages.to_sentence
+        redirect_to edit_admin_theme_path(@theme.id)
+      end
+    end
 
-		def destroy
-			@theme.destroy
-			flash[:success] = 'Vous avez bien supprimé ce Thème.'
-			redirect_to admin_themes_path
-		end
+    def destroy
+      @theme.destroy
+      respond_to do |format|
+        format.html do
+        end
+        format.js  do
+        end
 
-		private
+      end
+    end
 
-		def find_theme
-			@theme = Theme.find(params[:id])
-		end
+    private
 
-		def theme_params
-			params.require(:theme).permit(:title, :description)
-		end
+    def find_theme
+      @theme = Theme.find(params[:id])
+    end
 
-		def is_user_admin?
-			unless current_user.role == 'admin' && user_signed_in?
-				flash[:warning] = "Vous n'êtes pas Administrateur."
-				redirect_to root_path
-			end
-		end
+    def theme_params
+      params.require(:theme).permit(:title, :description)
+    end
 
-	end
+    def is_user_admin?
+      unless current_user.role == "admin" && user_signed_in?
+        flash[:warning] = "Vous n'êtes pas Administrateur."
+        redirect_to root_path
+      end
+    end
+  end
 end
-
