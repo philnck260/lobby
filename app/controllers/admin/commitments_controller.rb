bceac88c9d259b6c0ac56d18544fff0ff2078d6d
Daniel <daniel.venera2@gmail.com>
2020-03-25 11:06:02 +0100

    add ajax request for destroy in admin users (custom flash message)

--- app/controllers/admin/commitments_controller.rb
+++ app/controllers/admin/commitments_controller.rb
@@ -1,88 +1,86 @@
 module Admin
-	class CommitmentsController < ApplicationController
-		before_action :find_commitment, only: [:edit, :update, :destroy]
-		before_action :authenticate_user!
-		before_action :is_user_admin?
+  class CommitmentsController < ApplicationController
+    before_action :find_commitment, only: [:edit, :update, :destroy]
+    before_action :authenticate_user!
+    before_action :is_user_admin?
 
-		def index
-			@commitments = Commitment.all
-		end
+    def index
+      @commitments = Commitment.all
+    end
 
-		def new
-			@themes = themes_titles 
-			@commitment = Commitment.new
-			@commitment.commitment_themes.build 
+    def new
+      @themes = themes_titles
+      @commitment = Commitment.new
+      @commitment.commitment_themes.build
 =begin
 			@commitment.commitment_themes.each do |ct|
 				ct.build_theme
 			end
 =end
-		end
+    end
 
-		def create
-			@commitment = Commitment.new(commitment_params)
-			if @commitment.save
-				flash[:success] = "Vous avez créé un Mouvement.\nVous pouvez ajouter un second Thème au Mouvement créé en l'Éditant."
-				redirect_to admin_commitments_path
-			else
-				flash[:error] = @commitment.errors.full_messages.to_sentence
-				redirect_to new_admin_commitment_path
-			end
-		end
+    def create
+      @commitment = Commitment.new(commitment_params)
+      if @commitment.save
+        flash[:success] = "Vous avez créé un Mouvement.\nVous pouvez ajouter un second Thème au Mouvement créé en l'Éditant."
+        redirect_to admin_commitments_path
+      else
+        flash[:error] = @commitment.errors.full_messages.to_sentence
+        redirect_to new_admin_commitment_path
+      end
+    end
 
-		def edit
-			if @commitment.themes.count == 0 || @commitment.themes.count == 1
-				@commitment.commitment_themes.build
-			elsif @commitment.themes.count == 2
-				@checkbox = true
-			end
-		end
+    def edit
+      if @commitment.themes.count == 0 || @commitment.themes.count == 1
+        @commitment.commitment_themes.build
+      elsif @commitment.themes.count == 2
+        @checkbox = true
+      end
+    end
 
-		def update
-			if @commitment.update(commitment_params)
-				flash[:success] = 'Vous avez bien édité ce Mouvement'
-				redirect_to admin_commitments_path
-			else
-				flash[:error] = @commitment.errors.full_messages.to_sentence
-				redirect_to edit_admin_commitment_path(@commitment.id)
-			end
-		end
+    def update
+      if @commitment.update(commitment_params)
+        flash[:success] = "Vous avez bien édité ce Mouvement"
+        redirect_to admin_commitments_path
+      else
+        flash[:error] = @commitment.errors.full_messages.to_sentence
+        redirect_to edit_admin_commitment_path(@commitment.id)
+      end
+    end
 
-		def destroy
-			@commitment.destroy
-			respond_to do |format|
+    def destroy
+      @commitment.destroy
+      respond_to do |format|
         format.html do
         end
-        format.js  do
+        format.js do
         end
-
       end
-		end
+    end
 
-		private
+    private
 
-		def find_commitment
-			@commitment = Commitment.find(params[:id])
-		end
+    def find_commitment
+      @commitment = Commitment.find(params[:id])
+    end
 
-		def commitment_params
-			params.require(:commitment).permit(:title, :description, commitment_themes_attributes: [:id, :theme_id, :_destroy])
-		end
+    def commitment_params
+      params.require(:commitment).permit(:title, :description, commitment_themes_attributes: [:id, :theme_id, :_destroy])
+    end
 
-		def is_user_admin?
-			unless current_user.role == 'admin' && user_signed_in?
-				flash[:warning] = "Vous n'êtes pas Administrateur."
-				redirect_to root_path
-			end
-		end
+    def is_user_admin?
+      unless current_user.role == "admin" && user_signed_in?
+        flash[:warning] = "Vous n'êtes pas Administrateur."
+        redirect_to root_path
+      end
+    end
 
-		def themes_titles
-			titles_array = Array.new
-			Theme.all.each do |each_theme|
-				titles_array << each_theme.title
-			end	
-			return titles_array
-		end
-
-	end
+    def themes_titles
+      titles_array = Array.new
+      Theme.all.each do |each_theme|
+        titles_array << each_theme.title
+      end
+      return titles_array
+    end
+  end
 end
