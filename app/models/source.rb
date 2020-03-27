803b32af37b21c0a03e18c55cfba9e5dd5a4c049
axaidan <axaidan@hotmail.com>
2020-03-27 09:02:27 +0100

    multiple fix and corrections, routes.rb, admin edit and new views, theme and commitment shows views, addes length validations to source model

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,18 +1,15 @@
 class Source < ApplicationRecord
-	# CALLBACKS
-	
 	
 	# LINK
 	belongs_to :sourceable, polymorphic: true	 
 
 	# VALIDATIONS
-	validates :title, presence: true
-	validates :description, presence: true
+	validates :title, presence: true, length: {minimum: 10, maximum: 50}
+	validates :description, presence: true, length: {minimum: 75, maximum: 150}
 	validates :url, presence: true
 	validates :category, presence: true
 	validates :media, presence: true
 
 	# METHODS
-	
 
 end
