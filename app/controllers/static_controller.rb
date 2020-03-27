10e7aac20dea73402e957cba693c092d898ec88a
axaidan <axaidan@hotmail.com>
2020-03-27 10:25:35 +0100

    donate front and mailer corrected

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -17,7 +17,20 @@ class StaticController < ApplicationController
   end
 
   def donate
+		@total_stripe = total_stripe
     render params[:page]
   end
+
+	private
+
+	def total_stripe
+		total = 0
+		if Stripe::Charge.list
+			Stripe::Charge.list.each do |each_charge|
+				total += each_charge.amount / 100
+			end
+		end
+		return total
+	end
 
 end
